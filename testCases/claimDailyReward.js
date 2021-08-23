fc1695b7341671dcb4beb681e782288147e17fa1
Dennis Neumann <dennis2598@gmx.net>
2021-08-23 23:03:28 +0200

    added claimDailyReward case

---
+++ testCases/claimDailyReward.js
@@ -0,0 +1,20 @@
+Feature('claimDailyReward');
+
+const rewardUrl = process.env.reward_URL;
+const email = process.env.reward_user_email;
+const pwd = process.env.reward_user_pwd;
+
+Scenario('test something', ({ I }) => {
+    I.amOnPage(rewardUrl);
+    I.wait(2);
+    I.click('Log In');
+    I.wait(2);
+    I.fillField('input[type=email]', email);
+    I.fillField('input[type=password]', pwd);
+    I.pressKey('Enter');
+    I.click({class: 'rewardPageBtn'});
+    I.click({class: 'claimRewardBtn'});
+    I.click({class: 'closeBtn'});
+});
+
+// run command: npx codeceptjs run --steps
--- testCases/collectDailyReward.js
+++
@@ -1,9 +0,0 @@
-Feature('collectDailyReward');
-
-const rewardUrl = process.env.reward_URL;
-
-Scenario('test something', ({ I }) => {
-    I.amOnPage(rewardUrl);
-});
-
-// run command: npx codeceptjs run --steps
